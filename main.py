af97b57ba520557a25320940356f7d6849a4d64a
catchwhale <mrjoe8888@gmail.com>
2016-02-27 01:31:37 +0800

    Iwq

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def hello():
 def echo():
 	aa = request.form['text']
     	aa = aa.strip()
-	os.chdir('../lib')
+	os.chdir('lib')
     	os.system('./client_partner_detailsV2.py "' + aa + '"')
-    	aa = os.popen('cat ../lib/test2.json').read()
+    	aa = os.popen('cat test2.json').read()
     	return aa
 
 if __name__ == "__main__":
