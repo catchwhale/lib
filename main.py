f94f4ef8a27e3a96caede13de5f13e7b3079e172
catchwhale <mrjoe8888@gmail.com>
2016-02-27 01:01:15 +0800

    fd

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+sys.path.append('lib')
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    #create_file('naks')
+    return '<form method="POST">XPLAN ID<input name="text"><input type="submit" value="submit"></form>'
+
+@app.route("/", methods=['POST'])
+def echo():
+	aa = request.form['text']
+    	aa = aa.strip()
+	os.chdir('/etc/lib')
+    	os.system('./client_partner_detailsV2.py "' + aa + '"')
+    	aa = os.popen('cat /etc/test2.json').read()
+    	return aa
+
+if __name__ == "__main__":
+	while True:
+		app.run(host='0.0.0.0', port=8080, debug=True)
+
+#sudo kill -9 `ps -fA |grep hello | awk '{print $2}'`
