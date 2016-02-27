5e0302e8869649c985c95b3a461d71c04f540e20
catchwhale <mrjoe8888@gmail.com>
2016-02-27 10:02:45 +0800

    i

--- client_partner_detailsV2.py
+++ client_partner_detailsV2.py
@@ -305,8 +305,9 @@ def parse(parameter):
 		update_json('client_partner.json', myRec2)
 		len_ = get_json_key_val('client_partner.json')
 		# len_ = len_[0]
-		print len(len_[0])
-		# if len(len_) == 3:
+		# print len(len_[0])
+		if len(len_[0]) == 3:
+			print len_[1]
 		# 	data = get_json_key_val('client_partner.json')
 		# 	# data = data[1]
 		# 	print data[0]
